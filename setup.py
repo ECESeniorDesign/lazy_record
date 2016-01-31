b3dceae7eb144c9200041e6898a62b82f18eb016
Chase Conklin <chase@conklins.net>
2016-01-31 15:15:34 -0500

    0.4.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name='lazy_record',
   packages=['lazy_record', 'lazy_record/base'],
-  version='0.4.0',
+  version='0.4.1',
   description='Generic Model Layer for Python Web Applications using Sqlite3',
   author='Chase Conklin',
   author_email='cconkli4@u.rochester.edu',
   url='https://github.com/ECESeniorDesign/lazy_record',
   download_url='https://github.com/ECESeniorDesign/'
-               'lazy_record/tarball/0.4.0',
+               'lazy_record/tarball/0.4.1',
   keywords=['flask'],
   classifiers=[],
 )
