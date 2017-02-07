4f1908deb2ad1c0174042734482778e612836420
Chase Conklin <chase@conklins.net>
2017-02-07 17:49:10 -0500

    add ability to get information about a table's columns

--- lazy_record/schema.py
+++ lazy_record/schema.py
@@ -1,20 +1,28 @@
 import lazy_record.repo as repo
+from lazy_record.typecasts import datetime
+
+tables = {}
 
 class Schema:
-    def __init__(self, db):
+    def __init__(self, db, tables):
         self.db = db
+        self.tables = tables # Dictionary
 
     def createTable(self, name):
-        return CreateTable(name, self.db)
+        table = Table(name, self.db)
+        self.tables[name] = table
+        return table
 
     def dropTable(self, name):
         self.db.executescript("drop table {};".format(name))
+        del self.tables[name]
 
-class CreateTable:
+class Table:
     type_mappings = {
         "string": "text",
         "integer": "integer",
-        "float": "real"
+        "float": "real",
+        "timestamp": "timestamp",
     }
     def __init__(self, name, db):
         self.name = name
@@ -23,32 +31,76 @@ class CreateTable:
 
     def __getattr__(self, attr):
         def column(name, default=None, null=True):
-            column = [name, CreateTable.type_mappings[attr]]
-            if default is not None:
-                column.append("default {}".format(default))
-            if not null:
-                column.append("not null")
-            self.columns.append(" ".join(column))
-        if attr in CreateTable.type_mappings:
+            self.columns.append(Column(name, attr, default, null))
+        if attr in Table.type_mappings:
             return column
         return self.__getattribute__(attr)
 
+    # For creating tables only
     def __enter__(self):
         return self
     def __exit__(self, *args):
+        self.columns.extend([
+            Column("created_at", "timestamp", None, False),
+            Column("updated_at", "timestamp", None, False),
+        ])
         lines = [
             "create table {} (".format(self.name),
             "  id integer primary key autoincrement,",
-            "  " + ",\n  ".join(self.columns),
+            "  " + ",\n  ".join(str(col) for col in self.columns),
             ");"
         ]
         self.db.executescript("\n".join(lines))
+        self.columns.append(
+            Column("id", "integer", None, False)
+        )
 
+class Column:
+    casts = {
+        "string": str,
+        "integer": int,
+        "float": float,
+        "timestamp": datetime
+    }
+    def __init__(self, name, kind, default, null):
+        self.name = name
+        self.kind = kind
+        self.default = default
+        self.null = null
 
-def create(schema):
+    def __eq__(self, other):
+        return (self.name, self.kind, self.default, self.null) == \
+        (other.name, other.kind, other.default, other.null)
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __repr__(self):
+        return "{}({!r}, {!r}, {!r}, {!r})".format(type(self).__name__,
+                                                   self.name,
+                                                   self.kind,
+                                                   self.default,
+                                                   self.null)
+
+    def __str__(self):
+        column = [self.name, Table.type_mappings[self.kind]]
+        if self.default is not None:
+            column.append("default {}".format(self.default))
+        if not self.null:
+            column.append("not null")
+        return " ".join(column)
+
+    def cast(self, value):
+        return Column.casts[self.kind](value)
+
+def columns_for(table_name):
+    table = tables[table_name]
+    return {col.name: col for col in table.columns}
+
+def load(schema):
     with repo.Repo.db as db:
-        schema(db).up()
+        schema(db, tables).up()
 
 def drop(schema):
     with repo.Repo.db as db:
-        schema(db).down()
+        schema(db, tables).down()
--- test/test_schema.py
+++ test/test_schema.py
@@ -4,6 +4,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import lazy_record.schema as schema
+from lazy_record.typecasts import datetime
 
 class ExampleSchema(schema.Schema):
     def up(self):
@@ -11,6 +12,7 @@ class ExampleSchema(schema.Schema):
             t.string("foo", default=0, null=False)
             t.integer("bar") # Should default to NULL and allow NULLs
             t.float("baz", default=0.5) # allow NULLs
+            t.timestamp("bux")
 
     def down(self):
         self.dropTable("foos")
@@ -18,7 +20,7 @@ class ExampleSchema(schema.Schema):
 @mock.patch("lazy_record.schema.repo")
 class TestSchema(unittest.TestCase):
     def test_create_table(self, repo):
-        schema.create(ExampleSchema)
+        schema.load(ExampleSchema)
         repo.Repo.db.__enter__.assert_called_with()
         db = repo.Repo.db.__enter__.return_value
         script = "\n".join([
@@ -26,7 +28,10 @@ class TestSchema(unittest.TestCase):
             "  id integer primary key autoincrement,",
             "  foo text default 0 not null,",
             "  bar integer,",
-            "  baz real default 0.5",
+            "  baz real default 0.5,",
+            "  bux timestamp,",
+            "  created_at timestamp not null,",
+            "  updated_at timestamp not null",
             ");"
             ])
         db.executescript.assert_called_with(script)
@@ -37,6 +42,24 @@ class TestSchema(unittest.TestCase):
         db = repo.Repo.db.__enter__.return_value
         script = "drop table foos;"
         db.executescript.assert_called_with(script)
+
+    def test_get_columns_from_table(self, repo):
+        schema.load(ExampleSchema)
+        self.assertEqual(schema.columns_for("foos"),
+        {
+            "id":  schema.Column("id", kind="integer", default=None, null=False),
+            "foo": schema.Column("foo", kind="string", default=0, null=False),
+            "bar": schema.Column("bar", kind="integer", default=None, null=True),
+            "baz": schema.Column("baz", kind="float", default=0.5, null=True),
+            "bux": schema.Column("bux", kind="timestamp", default=None, null=True),
+            "created_at": schema.Column("created_at", kind="timestamp", default=None, null=False),
+            "updated_at": schema.Column("updated_at", kind="timestamp", default=None, null=False),
+        })
+
+    def test_cast_column_value(self, repo):
+        schema.load(ExampleSchema)
+        value = schema.columns_for("foos")["bar"].cast("5")
+        self.assertEqual(5, value)
 
 if __name__ == '__main__':
     unittest.main()
